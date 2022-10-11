bb3dff487143bb92ce3f484239bbfd2b7b6c3686
situ2001 <yongcong2001@outlook.com>
2022-10-11 12:12:43 +0800

    feat: add GitHub OAuth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { session } = pageProps as any;
+
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <SessionProvider session={session}>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </SessionProvider>
   );
 }
 
